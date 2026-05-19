e4445a4dc123b8cdd5a31b5bbd08f95efcb7b422
chiranjeevi <your-email@gmail.com>
2026-05-19 15:23:52 +0530

    debug origins

--- Backend/app.py
+++ Backend/app.py
@@ -56,9 +56,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-       "https://frontend-burst2.onrender.com"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
